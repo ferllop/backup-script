89f6935307ca16cdbc823616fc72aafe04bc609b
Ferran Llop <ferranllop@gmail.com>
2019-08-16 18:46:15 +0200

    Filesystem backup now starts from the root of the web filesytem instead of public_html.

--- backup.sh
+++ backup.sh
@@ -87,7 +87,7 @@ fi
 
 
 fs_backup_filename=${web}_fs_backup
-tar czvf ${backups_path}/${fs_backup_filename}.tar.gz ${exclude} /var/www/${web}/public_html/ 
+tar czvf ${backups_path}/${fs_backup_filename}.tar.gz ${exclude} /var/www/${web}/ 
 if [ $? -eq 0 ] 
 then
     #DAILY
