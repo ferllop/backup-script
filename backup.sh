ecdc596de48cf535981bf458cced9a08fea91a59
Ferran Llop <ferranllop@gmail.com>
2019-12-23 22:58:11 +0100

    feature: encryption of database dump sql file

--- backup.sh
+++ backup.sh
@@ -17,14 +17,16 @@ fulldate=$(date +%Y%m%dT%H.%M.%S)
 
 backups_path=/home/backup_user/backups
 db_backup_filename=${web}_db_backup
+
 mysqldump --user=backup_user --lock-tables --databases ${database} -h localhost > ${backups_path}/${db_backup_filename}.sql
 if [ $? -eq 0 ] 
 then
     some_cp_error="false"
-    gzip ${backups_path}/${db_backup_filename}.sql
+    xz --compress ${backups_path}/${db_backup_filename}.sql
+    gpg --symmetric --pinentry-mode loopback --passphrase-file ~/.gpg_key ${backups_path}/${db_backup_filename}.sql.xz
 
     #DAILY
-    cp ${backups_path}/${db_backup_filename}.sql.gz ${backups_path}/daily/${db_backup_filename}_daily_$(date +%A).sql.gz
+    cp ${backups_path}/${db_backup_filename}.sql.xz.gpg ${backups_path}/daily/${db_backup_filename}_daily_$(date +%A).sql.xz.gpg
         if [ $? -eq 0 ] 
         then
             db_message="Daily_DB_OK"
@@ -36,10 +38,10 @@ then
     #WEEKLY 5 backups
     if [ "$(date +%A)" == "Monday" ]
     then
-        cp ${backups_path}/${db_backup_filename}.sql.gz ${backups_path}/weekly/${db_backup_filename}_${fulldate}_weekly.sql.gz
+        cp ${backups_path}/${db_backup_filename}.sql.xz.gpg ${backups_path}/weekly/${db_backup_filename}_${fulldate}_weekly.sql.xz.gpg
         if [ $? -eq 0 ] 
         then
-            find ${backups_path}/weekly/${db_backup_filename}_*_weekly.sql.gz -mtime +35 -exec rm {} \;
+            find ${backups_path}/weekly/${db_backup_filename}_*_weekly.sql.xz.gpg -mtime +35 -exec rm {} \;
             db_message="${db_message}, Weekly_DB_OK"
         else
             db_message="${db_message}, Weekly_DB_FAILED"
@@ -50,10 +52,10 @@ then
     #MONTHLY 6 backups
     if [ "$(date +%d)" == "01" ]
     then
-        cp ${backups_path}/${db_backup_filename}.sql.gz ${backups_path}/monthly/${db_backup_filename}_monthly_$(date +%M).sql.gz
+        cp ${backups_path}/${db_backup_filename}.sql.xz.gpg ${backups_path}/monthly/${db_backup_filename}_monthly_$(date +%M).sql.xz.gpg
         if [ $? -eq 0 ] 
         then
-            find ${backups_path}/monthly/${db_backup_filename}_monthly_*.sql.gz -mtime +178 -exec rm {} \;
+            find ${backups_path}/monthly/${db_backup_filename}_monthly_*.sql.xz.gpg -mtime +178 -exec rm {} \;
           db_message="${db_message}, Monthly_DB_OK"
         else
             db_message="${db_message}, Monthly_DB_FAILED"
@@ -64,10 +66,10 @@ then
     #YEARLY 5 backups
     if [ "$(date +%j)" == "001" ]
     then
-        cp ${backups_path}/${db_backup_filename}.sql.gz ${backups_path}/yearly/${db_backup_filename}_yearly_$(date +%Y).sql.gz
+        cp ${backups_path}/${db_backup_filename}.sql.xz.gpg ${backups_path}/yearly/${db_backup_filename}_yearly_$(date +%Y).sql.xz.gpg
         if [ $? -eq 0 ] 
         then
-            find ${backups_path}/yearly/${db_backup_filename}_yearly_*.sql.gz -mtime +1825 -exec rm {} \;
+            find ${backups_path}/yearly/${db_backup_filename}_yearly_*.sql.xz.gpg -mtime +1825 -exec rm {} \;
           db_message="${db_message}, Yearly_DB_OK"
         else
             db_message="${db_message}, Yearly_DB_FAILED"
@@ -75,24 +77,28 @@ then
         fi
     fi
 
-    rm ${backups_path}/$db_backup_filename.sql.gz
-    
+    rm ${backups_path}/$db_backup_filename.sql.xz
+    rm ${backups_path}/$db_backup_filename.sql.xz.gpg
+
 else 
     if [ -e ${backups_path}/$db_backup_filename.sql ]
     then
         rm ${backups_path}/$db_backup_filename.sql
     fi
+
     db_message="DB_dump_FAILED"
     some_cp_error="true"
 fi  
 
 
 fs_backup_filename=${web}_fs_backup
-tar -C /var/www -cvJf ${backups_path}/${fs_backup_filename}.tar.xz ${exclude} ${web}
-gpg --symmetric --pinentry-mode loopback --passphrase-file ~/.gpg_key ${backups_path}/${fs_backup_filename}.tar.xz
 
-if [ $? -eq 0 ] 
+tar -C /var/www -cvJf ${backups_path}/${fs_backup_filename}.tar.xz ${exclude} ${web}
+
+if [ $? -eq 0 ]
 then
+    gpg --symmetric --pinentry-mode loopback --passphrase-file ~/.gpg_key ${backups_path}/${fs_backup_filename}.tar.xz
+    
     #DAILY
     cp ${backups_path}/${fs_backup_filename}.tar.xz.gpg ${backups_path}/daily/${fs_backup_filename}_daily_$(date +%A).tar.xz.gpg
         if [ $? -eq 0 ] 
@@ -145,15 +151,13 @@ then
         fi
     fi
 
-
     rm ${backups_path}/${fs_backup_filename}.tar.xz
     rm ${backups_path}/${fs_backup_filename}.tar.xz.gpg
 
 else 
-    if [ -e ${backups_path}/${fs_backup_filename}.tar.xz.gpg ] 
+    if [ -e ${backups_path}/${fs_backup_filename}.tar.xz ] 
     then
         rm ${backups_path}/${fs_backup_filename}.tar.xz
-        rm ${backups_path}/${fs_backup_filename}.tar.xz.gpg
     fi
     files_message="FS_TAR_FAILED"
     some_cp_error="true"
@@ -173,4 +177,4 @@ fi
 
 source /home/backup_user/.telegram_keys   
 URL=https://api.telegram.org/bot$TOKEN/sendMessage
-curl -s -X POST $URL -d chat_id=$CHANNEL -d text="${web} backup: ${db_message} and ${fs_message}. ${remote_message}" >/dev/null 2>&1
+curl -s -X POST $URL -d chat_id=$CHANNEL -d text="${web} backup: ${db_message} and ${fs_message}. ${remote_message}" > /dev/null 2>&1
